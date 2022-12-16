51df72e74816b7d329587ab54fefef8dd18e0593
Egor <60915234+egbusko@users.noreply.github.com>
2022-12-16 18:23:59 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import pydeck as pdk
+import time
+
+st.set_page_config(layout="wide")
 
 DATA_URL = (
 "Motor_Vehicle_Collisions_-_Crashes.csv"
@@ -9,7 +12,18 @@ DATA_URL = (
 
 st.title("Motor Vehicle Collisions in New York City 🗽")
 st.markdown("This application is a Streamlit dashboard that can be used "
-"to analyze motor vehicle collisions in NYC🚗")
+"to analyze motor vehicle collisions in NYC  \n"
+"*Data Provided by Police Department (NYPD)* 👮🏼")
+
+
+with st.expander('About this dataset'):
+  st.write('The Motor Vehicle Collisions crash table contains details on the '
+  'crash event. Each row represents a crash event. The Motor Vehicle Collisions '
+  'data tables contain information from all police reported motor vehicle '
+  'collisions in NYC. The police report (MV104-AN) is required to be filled out '
+  'for collisions where someone is injured or killed, or where there is at least '
+  '$1000 worth of damage.')
+
 
 @st.cache(persist=True)
 def load_data(nrows):
@@ -23,17 +37,27 @@ def load_data(nrows):
 data = load_data(100000)
 original_data = data
 
+
+my_bar = st.progress(0)
+
+for percent_complete in range(100):
+     time.sleep(0.01)
+     my_bar.progress(percent_complete + 1)
+
+
 st.header("Where are the most people injured in NYC?")
-injured_people = st.slider("Number of people injured in vechicle collisions 💥", 0, 19)
+st.sidebar.header("Settings")
+injured_people = st.sidebar.slider("🧍 Number of people injured in vechicle collisions:", 0, 19)
 st.map(data.query("injured_persons >= @injured_people")[["latitude", "longitude"]].dropna(how="any"))
 
 
 st.header("How many collisions occur during a given time of day?")
-hour = st.slider("Hour to look at", 0, 23)
-data = data[data['date/time'].dt.hour == hour]
-# st.header(data.columns)
+hours = st.sidebar.slider("⌚ Hour to look at:", 0, 23, (3,6))
+data = data[
+    (data['date/time'].dt.hour >= hours[0]) & (data['date/time'].dt.hour <= hours[1])
+]
 
-st.markdown("Vehicle collisions between %i:00 and %i:00" % (hour, (hour + 1) % 24))
+st.markdown("Vehicle collisions between %i:00 and %i:00 🚗💥" % (hours[0], hours[1] % 24))
 midpoint = (np.average(data['latitude']), np.average(data['longitude']))
 
 st.write(pdk.Deck(
@@ -58,18 +82,16 @@ st.write(pdk.Deck(
     ]
 ))
 
-st.subheader("Breakdown by minute between %i:00 and %i:00" % (hour, (hour + 1) % 24))
-filtered = data[
-    (data['date/time'].dt.hour >= hour) & (data['date/time'].dt.time)
-]
+st.subheader("Breakdown by minute between %i:00 and %i:00" % (hours[0], hours[1] % 24))
+
+filtered = data
 hist = np.histogram(filtered['date/time'].dt.minute, bins=60, range=(0, 60))[0]
 chart_data = pd.DataFrame({'minute': range(60), 'crashes':hist})
 st.bar_chart(chart_data, x = 'minute', height=400, use_container_width=True)
-# fig = px.bar(chart_data, x = 'minute', y='crashes', hover_data=['minute', 'crashes'], height=400)
-# st.write(fig)
+st.write("Note: looks like the police tend to record accidents rounding the time to the nearest five minutes.")
 
 st.header("Top 5 dangerous streets by affected type")
-select = st.selectbox('Affected type of people', ['Pedestrians 🚶', 'Cyclists 🚴', 'Motorists 👨‍🦽'])
+select = st.selectbox('Affected type of people:', ['Pedestrians 🚶', 'Cyclists 🚴', 'Motorists 👨‍🦽'])
 
 if select == 'Pedestrians 🚶':
     st.write(original_data.query("injured_pedestrians >= 1")[["on_street_name", "injured_pedestrians"]].sort_values(by=['injured_pedestrians'], ascending=False).dropna(how='any')[:5])
@@ -79,7 +101,6 @@ elif select == 'Cyclists 🚴':
 
 else:
     st.write(original_data.query("injured_motorists >= 1")[["on_street_name", "injured_motorists"]].sort_values(by=['injured_motorists'], ascending=False).dropna(how='any')[:5])
-
 
 
 
